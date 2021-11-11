3c04fabad655236ed2db69948223e2d923938958
Inug <nugrahadi.tristya@ti.ukdw.ac.id>
2021-11-11 18:28:27 +0700

    3_A_71210677

---
+++ 3_A_71210677.py
@@ -0,0 +1,8 @@
+#Membuat daftar belanja
+
+a1 = input("Masukan daftar belanja 1: ")
+a2 = input("Masukan daftar belanja 2: ")
+db1 = input("Tambahan data ke daftar belanja 1 : ")
+db2 = input("Tambahkan data ke daftar belanja 2 :")
+print("Daftar belanja 1 adalah " , a1 , "," ,db1)
+print("Daftar belanja 2 adalah ", a2, "," ,db2)
